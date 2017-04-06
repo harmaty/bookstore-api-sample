2b72131db1907d5e02fbc61a8cc2d635cbe3edb7
Artem Harmaty <harmaty@gmail.com>
2017-04-06 14:35:28 +0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'john@mail.com', first_name: 'John', last_name: 'Smith')
+
+Book.create([
+                {title: 'Book Title 1', isbn: '11111-1111-1111', author: 'Author 1'},
+                {title: 'Book Title 2', isbn: '22222-2222-2222', author: 'Author 2'},
+                {title: 'Book Title 3', isbn: '33333-3333-3333', author: 'Author 3'},
+                {title: 'Book Title 4', isbn: '44444-4444-4444', author: 'Author 4'},
+                {title: 'Book Title 5', isbn: '55555-5555-5555', author: 'Author 5'}
+            ])
